448220db55dc4ca67c9ff7ef044388ccdb80d6ed
nvs2394 <nvs2394@gmail.com>
2016-03-08 13:50:42 +0700

    init

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
